fa9b8db3e3234fd03de7874bd88920336b7da135
Giada <giada@Giadas-MacBook-Pro.local>
2025-06-26 12:32:07 +0200

    Fix CORS headers per permettere richieste da Systeme.io

--- api/comment-webhook.js
+++ api/comment-webhook.js
@@ -36,10 +36,10 @@ module.exports = async (req, res) => {
     console.log('🔥 Richiesta:', req.method, req.url);
     
     // Abilita CORS per permettere richieste dal browser
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-    
+res.setHeader('Access-Control-Allow-Origin', '*');
+res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Accept, Origin');
+res.setHeader('Access-Control-Allow-Credentials', 'true');
     // Gestisci preflight OPTIONS
     if (req.method === 'OPTIONS') {
         return res.status(200).end();
